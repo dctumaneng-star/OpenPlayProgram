cae8c0834f22e844cca9bfb41847a5fe8ddfad3d
daryltumaneng <239285206+spoterdarylhan-star@users.noreply.github.com>
2026-09-15 15:43:38 +0800

    google login fix

--- openplay-web/src/App.jsx
+++ openplay-web/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
-import { GoogleLogin } from '@react-oauth/google';
+import { GoogleLogin, useGoogleLogin } from '@react-oauth/google';
 import {
   BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip,
   Legend, ResponsiveContainer, PieChart, Pie, Cell
